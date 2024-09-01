bb9f9d85a2e825a9aecdf04e1a5d075a0a0b71b6
Nitesh bakhla <143624783+Niteshbakhla@users.noreply.github.com>
2024-09-01 16:29:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ connectDB();
 app.use(express.json());
 app.use(cors(
             {
-                        origin: "https://pic-frontend-tau.vercel.app",
+                        origin: "*",
                         credentials: true
             }
 ));
